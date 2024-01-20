9eb9094690513601165e457a3fc62bada7f60897
Jeremiah Givens <jeremiahgivens149@gmail.com>
2024-01-20 16:43:43 -0600

    Created supporting classes to handle main tasks. Added more methods to MainWindowExtension to handle input.

---
+++ SupportClasses/ImageAnnotator.py
@@ -0,0 +1,3 @@
+
+class ImageAnnotator():
+    pass
---
+++ SupportClasses/OutputHandler.py
@@ -0,0 +1,13 @@
+
+class OutputHandler():
+    def setOutputFolderPath(self, path):
+        self.ouptputFolderPath = path
+
+    def addImageToFolder(self, path):
+        pass
+
+    def buildOutputFolder(self):
+        pass
+
+    def setOutputFormat(self, format):
+        pass
