e023e886c8c938cb4b525b650e74222c791d4ed1
Jeremiah Givens <jeremiahgivens149@gmail.com>
2024-03-10 14:04:15 -0500

    Image now resizes as window size changes.

--- SupportClasses/ImageAnnotator.py
+++ SupportClasses/ImageAnnotator.py
@@ -9,17 +9,26 @@ class ImageAnnotator():
         self.graphicsView = graphicsView
         self.scene = QGraphicsScene()
         self.graphicsView.setScene(self.scene)
+        self.image_qt = None
+        self.pic = None
+
+    def resizeImage(self):
+        if self.image_qt is not None and self.pic is not None:
+            pixMap = QPixmap.fromImage(self.image_qt)
+            gvSize = self.graphicsView.size()
+            pixMap = pixMap.scaled(gvSize.width(), gvSize.height(), Qt.AspectRatioMode.KeepAspectRatio)
+            self.pic.setPixmap(pixMap)
+            print("Resizing")
 
     def load(self):
         path = self.outputHandler.getPathToFirstImage()
         if os.path.exists(path):
             self.image_qt = QImage(path)
-            pic = QGraphicsPixmapItem()
+            self.pic = QGraphicsPixmapItem()
             pixMap = QPixmap.fromImage(self.image_qt)
             gvSize = self.graphicsView.size()
             pixMap = pixMap.scaled(gvSize.width(), gvSize.height(), Qt.AspectRatioMode.KeepAspectRatio)
-            pic.setPixmap(pixMap)
-            self.scene.addItem(pic)
-            print("Image added to graphics view.")
+            self.pic.setPixmap(pixMap)
+            self.scene.addItem(self.pic)
         else:
             print("Issue loading image in graphics view. See ImageAnnotator load method.")
